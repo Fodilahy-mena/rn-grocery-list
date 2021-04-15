21a331d21929d5e570924af6b639bef051cebc0f
JEROME Emilien <jerome.emi@onja.org>
2021-04-15 14:39:29 +0300

    added state to add new item in the list

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import { View, Text, SafeAreaView, ScrollView} from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, SafeAreaView, ScrollView, FlatList, KeyboardAvoidingView} from 'react-native'
 import nachos from '../data/nachos';
+import {v4 as uuid } from 'uuid';
 
 import ListItem, {Separator} from '../components/ListItem';
+import AddItem from '../components/AddItem';
 
 export default () => {
+    const [list, setList] = useState(nachos)
     return (
-        <SafeAreaView>
-            <ScrollView>
-                {nachos.map((item, index)=> (
-                    <React.Fragment key={item.id}>
+        <SafeAreaView style={{flex: 1,}}>
+            <KeyboardAvoidingView 
+                style={{flex:1,}}
+                behavior="padding">
+                <FlatList 
+                    data={list}
+                    renderItem={({item, index}) => (
                         <ListItem 
                             name={item.name}
                             onFavoritePress={() => alert("todo: handle favorite")}
-                            isFavorite={index < 2}/>
-                        <Separator/>
-                    </React.Fragment>
-                ))}
-            </ScrollView>
+                            isFavorite={index < 2}
+                        />
+                    )}
+                    keyExtractor={(item) => item.id}
+                    ItemSeparatorComponent={() => <Separator />}
+                    ListHeaderComponent= {() => <AddItem onSubmitEditing={({ nativeEvent: {text}}) => {
+                        setList([{id: uuid(), name: text}, ...list])
+                    }}/>}
+                />
+            </KeyboardAvoidingView>
         </SafeAreaView>
 
     )
+    // return (
+    //     <SafeAreaView>
+    //         <ScrollView>
+    //             {nachos.map((item, index)=> (
+    //                 <React.Fragment key={item.id}>
+    //                     <ListItem 
+    //                         name={item.name}
+    //                         onFavoritePress={() => alert("todo: handle favorite")}
+    //                         isFavorite={index < 2}/>
+    //                     <Separator/>
+    //                 </React.Fragment>
+    //             ))}
+    //         </ScrollView>
+    //     </SafeAreaView>
+
+    // )
 };
 
 
